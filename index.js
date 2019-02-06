4a263a11f8d8059e38d63615f220864592333321
sarahcalande <scalande@yahoo.com>
2019-02-06 09:43:16 -0500

    add

---
+++ index.js
@@ -0,0 +1,4 @@
+curl -X POST -H 'Content-Type: application/json' -H
+'Authorization: Bearer YOUR_API_TOKEN'
+-d '{"name":"YOUR NAME","emails":[{"email":"youremail@yourdomain.com"}]}'
+https://api.kustomerapp.com/v1/customers
